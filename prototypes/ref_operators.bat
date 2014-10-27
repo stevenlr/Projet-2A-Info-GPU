58b67388b3ad1d0540ed2657b9f7276c4747d156
Steven Le Rouzic <steven.lerouzic@gmail.com>
2014-10-27 16:35:29 +0100

    renames source images

--- prototypes/ref_operators.bat
+++ prototypes/ref_operators.bat
@@ -1,7 +1,7 @@
-pinverse Lenna.pan ref-invert.pan
-padd Lenna.pan DS.pan ref-add.pan
-pbinarization 127 255 Lenna.pan ref-threshold.pan
-perosion 1 3 binary.pan ref-erosion-binary.pan
-perosion 1 1 Lenna.pan ref-erosion-colors.pan
-pconvolution matrices/7x7gaussian.txt Lenna.pan ref-convolution-7x7gaussian.pan
-pconvolution matrices/3x3sharpen.txt Lenna.pan ref-convolution-3x3sharpen.pan
+pinverse src-Lenna.pan ref-invert.pan
+padd src-Lenna.pan src-DS.pan ref-add.pan
+pbinarization 127 255 src-Lenna.pan ref-threshold.pan
+perosion 1 3 src-binary.pan ref-erosion-binary.pan
+perosion 1 1 src-Lenna.pan ref-erosion-colors.pan
+pconvolution matrices/7x7gaussian.txt src-Lenna.pan ref-convolution-7x7gaussian.pan
+pconvolution matrices/3x3sharpen.txt src-Lenna.pan ref-convolution-3x3sharpen.pan
--- prototypes/test_operators.bat
+++ prototypes/test_operators.bat
@@ -1,9 +1,9 @@
-proto-invert.exe Lenna.pan out-invert.pan
-proto-add.exe Lenna.pan DS.pan out-add.pan
-proto-threshold.exe 127 Lenna.pan out-threshold.pan
-proto-erosion.exe 3 binary.pan out-erosion-binary.pan
-proto-erosion.exe 1 Lenna.pan out-erosion-colors.pan
-proto-convolution.exe matrices/7x7gaussian.txt 0 Lenna.pan out-convolution-7x7gaussian-warp.pan
-proto-convolution.exe matrices/7x7gaussian.txt 1 Lenna.pan out-convolution-7x7gaussian-extend.pan
-proto-convolution.exe matrices/7x7gaussian.txt 2 Lenna.pan out-convolution-7x7gaussian-crop.pan
-proto-convolution.exe matrices/3x3sharpen.txt 1 Lenna.pan out-convolution-3x3sharpen-extend.pan
+proto-invert.exe src-Lenna.pan out-invert.pan
+proto-add.exe src-Lenna.pan src-DS.pan out-add.pan
+proto-threshold.exe 127 src-Lenna.pan out-threshold.pan
+proto-erosion.exe 3 src-binary.pan out-erosion-binary.pan
+proto-erosion.exe 1 src-Lenna.pan out-erosion-colors.pan
+proto-convolution.exe matrices/7x7gaussian.txt 0 src-Lenna.pan out-convolution-7x7gaussian-warp.pan
+proto-convolution.exe matrices/7x7gaussian.txt 1 src-Lenna.pan out-convolution-7x7gaussian-extend.pan
+proto-convolution.exe matrices/7x7gaussian.txt 2 src-Lenna.pan out-convolution-7x7gaussian-crop.pan
+proto-convolution.exe matrices/3x3sharpen.txt 1 src-Lenna.pan out-convolution-3x3sharpen-extend.pan
