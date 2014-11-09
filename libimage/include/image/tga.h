63b734a9c30ad604530483c07f7cd5cc8c778846
Steven Le Rouzic <steven.lerouzic@gmail.com>
2014-11-09 16:35:14 +0100

    libimage: moves tga structure to source file

--- libimage/include/image/tga.h
+++ libimage/include/image/tga.h
@@ -7,52 +7,16 @@
 #ifndef _TGA_H
 #define _TGA_H
 
-#include <stdint.h>
 #include "image/image.h"
 
-#pragma pack(push)
-#pragma pack(1)
-
 /**
- * Should be unused.
+ * Reads a Truevision Targa image.
+ * @param filename Path to the image to open.
+ * @param imageptr Pointer to the image to create.
+ * @return 0 if the image was read successfully.
+ *         1 if there was an error when allocating memory,
+ *	   2 if arguments were invalid.
  */
-struct TGAColorMapSpecification {
-	uint16_t first_entry_index;
-	uint16_t color_map_length;
-	uint8_t color_map_entry_size;
-};
-
-struct TGAImageSpecification {
-	uint16_t x_origin;
-	uint16_t y_origin;
-
-	uint16_t width;
-	uint16_t height;
-
-	uint8_t pixel_depth;
-	uint8_t image_descriptor;
-};
-
-struct TGAHeader {
-
-	uint8_t id_length;
-
-	/**
-	 * Should always be 0. (No color map)
-	 */
-	uint8_t color_map_type;
-
-	/**
-	 * 2 : uncompressed color-mapped image.
-	 * 3 : uncompressed grayscale image.
-	 * other : not supported.
-	 */
-	uint8_t image_type;
-
-	struct TGAColorMapSpecification color_map_specification;
-	struct TGAImageSpecification image_specification;
-};
-
-#pragma pack(pop)
+int TGA_readImage(const char *filename, Image **imageptr);
 
 #endif
--- libimage/src/tga.c
+++ libimage/src/tga.c
@@ -1,11 +1,56 @@
 /**
- * @file image.c
+ * @file tga.c
  * @author Gautier Boëda <boeda@ecole.ensicaen.fr>
  * @author Steven Le Rouzic <lerouzic@ecole.ensicaen.fr>
  */
 
 #include <stdio.h>
 #include <stdlib.h>
+ #include <stdint.h>
 
 #include "image/tga.h"
 
+#pragma pack(push)
+#pragma pack(1)
+
+/**
+ * Should be unused.
+ */
+struct TGAColorMapSpecification {
+	uint16_t first_entry_index;
+	uint16_t color_map_length;
+	uint8_t color_map_entry_size;
+};
+
+struct TGAImageSpecification {
+	uint16_t x_origin;
+	uint16_t y_origin;
+
+	uint16_t width;
+	uint16_t height;
+
+	uint8_t pixel_depth;
+	uint8_t image_descriptor;
+};
+
+struct TGAHeader {
+
+	uint8_t id_length;
+
+	/**
+	 * Should always be 0. (No color map)
+	 */
+	uint8_t color_map_type;
+
+	/**
+	 * 2 : uncompressed color-mapped image.
+	 * 3 : uncompressed grayscale image.
+	 * other : not supported.
+	 */
+	uint8_t image_type;
+
+	struct TGAColorMapSpecification color_map_specification;
+	struct TGAImageSpecification image_specification;
+};
+
+#pragma pack(pop)
