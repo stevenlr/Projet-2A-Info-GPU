c64bb490efe98751c645002aef75934079916454
Steven Le Rouzic <steven.lerouzic@gmail.com>
2014-11-13 23:54:07 +0100

    libimage: moar tests

--- libimage/test/test_image.c
+++ libimage/test/test_image.c
@@ -8,6 +8,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdint.h>
+#include <time.h>
 #include <image/image.h>
 #include <unittest.h>
 
@@ -37,7 +38,34 @@ void test_image_new_delete()
 	ut_assert("NULL data image deletion", Image_delete(&img2) == 1);
 }
 
+void test_image_copy()
+{
+	Image *img1, *img2;
+
+	Image_new(8, 8, 1, &img1);
+	srand(time(NULL));
+
+	for (int i = 0; i < img1->width * img1->height; ++i) {
+		img1->data[i] = rand() % 256;
+	}
+
+	ut_assert("copy null src image", Image_copy(NULL, &img2) == 2);
+	ut_assert("copy null dst image", Image_copy(img1, NULL) == 2);
+	ut_assert("copy image", Image_copy(img1, &img2) == 0);
+
+	ut_assert("copy image width", img1->width == img2->width);
+	ut_assert("copy image height", img1->height == img2->height);
+	ut_assert("copy image channels", img1->channels == img2->channels);
+
+	ut_assert("copy image data", memcmp(img1->data, img2->data, img1->width * img1->height * sizeof(uint8_t)) == 0);
+
+	Image_delete(img1);
+	Image_delete(img2);
+}
+
 void test_image_all()
 {
 	test_image_new_delete();
+	test_image_copy();
+	test_ulage_data();
 }
