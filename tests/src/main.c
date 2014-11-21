2c929aef040e99d43aea3648fe1d83410f9b9f7b
Goutye <gaut.boeda@gmail.com>
2014-11-21 01:42:01 +0100

    tests: finished

---
+++ tests/src/main.c
@@ -0,0 +1,32 @@
+/**
+ * @file main.c
+ * @author Steven Le Rouzic <lerouzic@ecole.ensicaen.fr>
+ * @author Gautier Boëda <boeda@ecole.ensicaen.fr>
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include "tests.h"
+
+int main(int argc, char *argv[])
+{
+	if (argc < 2) {
+		printf("Usage: %s <type> <filter1> <filter2> <filtern>", argv[0]);
+		return 0;
+	}
+
+	if (strcmp(argv[1], "all") == 0) {
+		launchTestsOf("gpp", argv + 2, argc - 2);
+	} else if (strcmp(argv[1], "gpp") == 0) {
+		launchTestsOf("gpp", argv + 2, argc - 2);
+	} else if (strcmp(argv[1], "cuda") == 0) {
+		printf("Not implemented.\n");
+	} else if (strcmp(argv[1], "opencl") == 0) {
+		printf("Not implemented.\n");
+	} else {
+		printf("Type not found.\n");
+	}
+
+	return 0;
+}
