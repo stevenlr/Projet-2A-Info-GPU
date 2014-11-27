78384787fbef016ed1fa85bc5e6b2ef2c7104f1a
Goutye <gaut.b@sfr.fr>
2014-11-27 20:38:56 +0100

    Tests: extensible version. Todo: Makefile target run (debug)

--- tests/src/main.c
+++ tests/src/main.c
@@ -7,25 +7,70 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
-#include "tests.h"
+#include <image/image.h>
+#include <image/tga.h>
 
 int main(int argc, char *argv[])
 {
-	if (argc < 2) {
-		printf("Usage: %s <type> <filter1> <filter2> <filtern>", argv[0]);
+	if (argc < 3) {
+		printf("Usage: %s <type> <image1> <image2>", argv[0]);
 		return 0;
 	}
 
-	if (strcmp(argv[1], "all") == 0) {
-		launchTestsOf("gpp", argv + 2, argc - 2);
-	} else if (strcmp(argv[1], "gpp") == 0) {
-		launchTestsOf("gpp", argv + 2, argc - 2);
-	} else if (strcmp(argv[1], "cuda") == 0) {
-		printf("Not implemented.\n");
-	} else if (strcmp(argv[1], "opencl") == 0) {
-		printf("Not implemented.\n");
+	Image *ref_image;
+	Image *test_image;
+	int error = 0;
+	char filePathReference[100];
+	char filePathTest[100];
+
+	sprintf(filePathReference, argv[1]);
+	sprintf(filePathTest, argv[2]);
+
+	if ((error = TGA_readImage(filePathReference, &ref_image)) != 0) {
+		printf("Error when opening image: %d (%s)\n", error,
+			filePathReference);
+		printf("%s", filePathReference);
+		return 0;
+	}
+
+	if ((error = TGA_readImage(filePathTest, &test_image)) != 0) {
+		printf("Error when opening image: %d (%s)\n", error, filePathTest);
+		printf("%s", filePathTest);
+		return 0;
+	}
+
+	if (test_image->height != ref_image->height ||
+		test_image->width != ref_image->width ) {
+		printf("Error : Images should be the same size.");
+		return 0;
+	}
+
+	int size, i;
+	uint8_t *datar, *datat, datad;
+	float averageDifference = 0;
+
+	datar = ref_image->data;
+	datat = test_image->data;
+
+	size = ref_image->width * ref_image->height * ref_image->channels;
+
+	for (i = 0; i < size; ++i) {
+
+		datad = abs((*datar++) - (*datat++));
+
+		if (datad > 0) {
+			averageDifference += datad;
+			++error;
+		}
+	}
+
+	float differencePercent = ((float) error) / (ref_image->height * ref_image->width) * 100;
+	averageDifference /= (float) (ref_image->height * ref_image->width) * 100;
+
+	if (error == 0) {
+		printf("OK!\n");
 	} else {
-		printf("Type not found.\n");
+		printf("Failed! (Difference : %.3f%%) (Average difference per pixel : %.3f)\n", differencePercent, averageDifference);
 	}
 
 	return 0;
