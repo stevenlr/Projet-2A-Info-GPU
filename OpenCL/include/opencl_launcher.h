4a7ac48fa84a6586e339182daadebeed6ff54164
Goutye <gaut.b@sfr.fr>
2015-03-15 22:41:32 +0100

    OpenCL: updated version of Opencl_launcher header

--- OpenCL/include/opencl_launcher.h
+++ OpenCL/include/opencl_launcher.h
@@ -1,12 +1,15 @@
 #include <CL/opencl.h>
+#include <iostream>
 #include <string>
 
-class Opencl_launcher(){
+class Opencl_launcher{
 public:
-	Opencl_launcher();
-	Boolean Opencl_load_kernel(String file);
+	Opencl_launcher(std::string name);
+	~Opencl_launcher();
+	cl_kernel load_kernel(std::string file, std::string name);
 	cl_context getContext();
-	cl_queue getQueue();
+	cl_command_queue getQueue();
+	void benchmark(cl_event event, std::string name);
 private:
 	cl_int error = 0;
 	cl_platform_id platform;
@@ -15,4 +18,4 @@ private:
 	cl_command_queue queue;
 	cl_device_id device;
 	cl_kernel kernel;
-}
+};
